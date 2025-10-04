5920bc71893c9f519295d018214d58ea05b87f8d
rsantoss91 <robertayoongi60@gmail.com>
2025-10-04 19:25:39 -0300

    Update disciplina.py

--- estudos/models/disciplina.py
+++ estudos/models/disciplina.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Disciplina(models.Model):
-    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
-    nome = models.CharField()
-    descricao = models.TextField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="disciplinas")
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nome
