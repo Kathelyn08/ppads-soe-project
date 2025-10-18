9d56d9edd28015545c2c332b4baa7fc91dccf8e9
rsantoss91 <robertayoongi60@gmail.com>
2025-10-18 20:39:19 -0300

    Update urls.py

--- estudos/urls.py
+++ estudos/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import DisciplinaViewSet, AtividadeViewSet
+from api.views import DisciplinaViewSet, AtividadeViewSet, cadastrar_usuario
 
 router = DefaultRouter()
 router.register(r"disciplinas", DisciplinaViewSet, basename="disciplinas")
 router.register(r"atividades", AtividadeViewSet, basename="atividades")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("cadastrar/", cadastrar_usuario, name="cadastrar_usuario"),
+]
+
+urlpatterns += router.urls
