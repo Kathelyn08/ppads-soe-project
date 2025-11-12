5551a2ce4bb5df1edc7d84dc9a482632b3370a56
Yasmin Lins <yasminlins.sites@gmail.com>
2025-11-11 22:46:30 -0300

    adição de coluna em disciplina

--- estudos/models/disciplina.py
+++ estudos/models/disciplina.py
@@ -5,6 +5,7 @@ class Disciplina(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="disciplinas")
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True)
+    criado_em = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.nome
