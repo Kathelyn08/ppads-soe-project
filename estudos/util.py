958c9b506358a2916c5a7243d82ea37145d2de34
Yasmin Lins <yasminlins.sites@gmail.com>
2025-11-20 22:11:01 -0300

    migrations

--- estudos/util.py
+++ estudos/util.py
@@ -3,7 +3,7 @@ from datetime import date, datetime, timedelta
 def week_range():
     now = datetime.now()
     week_start = (now - timedelta(days=now.weekday() + 1)).replace(hour=0, minute=0, second=0, microsecond=0)
-    week_end = week_start + timedelta(days=6, hours=23, minutes=59, seconds=59)
+    week_end = now + timedelta(days=6, hours=23, minutes=59, seconds=59)
 
     return week_start, week_end
 
