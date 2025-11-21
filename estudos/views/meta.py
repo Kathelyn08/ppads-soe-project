60c60978bed1c26d7b12037fb3b6a24700e23973
Yasmin Lins <yasminlins.sites@gmail.com>
2025-11-20 22:12:59 -0300

    ajuste

--- estudos/views/meta.py
+++ estudos/views/meta.py
@@ -71,8 +71,8 @@ class MetaListView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = self.form
         context['total'] = len(context['metas'])
-        #context['concluido'] = len([meta for meta in context['metas'] if meta.concluida()])
-        #context['progresso'] = context['concluido'] / (context['total'] or 1) * 100
+        context['concluido'] = len([meta for meta in context['metas'] if meta.concluida()])
+        context['progresso'] = context['concluido'] / (context['total'] or 1) * 100
 
         return context
 
